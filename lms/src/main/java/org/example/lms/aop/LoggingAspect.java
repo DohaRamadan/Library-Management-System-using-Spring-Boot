bc83e1c229c1dc899872e4b2b9aab454c5006d11
DohaRamadan <77820526+DohaRamadan@users.noreply.github.com>
2024-04-28 12:08:17 +0300

    refactored aop

--- lms/src/main/java/org/example/lms/aop/LoggingAspect.java
+++ lms/src/main/java/org/example/lms/aop/LoggingAspect.java
@@ -1,46 +1,40 @@
 package org.example.lms.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    private Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
-    private final ThreadLocal<Long> startTime = new ThreadLocal<>();
+    private final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* org.example.lms.controllers.*.*(..)) || " +
+    @Around("execution(* org.example.lms.controllers.*.*(..)) || " +
             "execution(* org.example.lms.services.*.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        logger.info("Entering method: " + joinPoint.getSignature().toShortString());
-        startTime.set(System.currentTimeMillis());
-    }
+    public Object logBeforeAndAfter(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        logger.info("Entering method: " + proceedingJoinPoint.getSignature().toShortString());
 
-    @AfterReturning(pointcut = "execution(* org.example.lms.controllers.*.*(..)) || " +
-            "execution(* org.example.lms.services.*.*(..))", returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        Long start = startTime.get();
-        if (start != null) {
-            long endTime = System.currentTimeMillis();
-            long executionTime = endTime - start;
-            logger.info("Exiting method: " + joinPoint.getSignature().toShortString() + ". Execution time: " + executionTime + "ms");
-            startTime.remove();
-        } else {
-            logger.warn("Exiting method: " + joinPoint.getSignature().toShortString() + ". Could not determine execution time.");
-        }
+        final StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        logger.info("Exiting method: " + proceedingJoinPoint.getSignature().toShortString() + ". Execution time: " + stopWatch.getTotalTimeMillis() + "ms");
+
+        return result;
     }
 
     @AfterThrowing(pointcut = "execution(* org.example.lms.controllers.*.*(..)) || " +
             "execution(* org.example.lms.services.*.*(..))", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
         logger.error("Exception in method: " + joinPoint.getSignature().toShortString() + ". Exception message: " + exception.getMessage());
-        startTime.remove();
     }
 }
